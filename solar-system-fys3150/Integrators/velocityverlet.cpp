8c25f785d805bbaac12b5fb8748f86302a564e24
Sarah <sarah.rastad@gmail.com>
2016-10-21 12:28:41 +0200

    euler+verlet funker

--- solar-system-fys3150/Integrators/velocityverlet.cpp
+++ solar-system-fys3150/Integrators/velocityverlet.cpp
@@ -1,5 +1,6 @@
 #include "velocityverlet.h"
 #include "../system.h"
+#include <iostream>
 
 VelocityVerlet::VelocityVerlet(System* system)
     : Integrator(system) {
@@ -11,11 +12,15 @@ std::string VelocityVerlet::getName() {
 
 void VelocityVerlet::integrateOneStep(std::vector<Particle*> particles) {
     for (int i = 0; i < particles.size(); i++) {
-        //Particle *p = particles.at(i);
+        Particle *p = particles.at(i);
         //vec3 pos = p->getPosition();
         //vec3 vel = p->getVelocity();
-        //double dt = Integrator.getDt();
-
-
+        double dt = getDt();
+        double m = p->getMass();
+        vec3 a1 = p->getForce()/m;
+        p->getPosition().operator +=(dt*p->getVelocity().operator +=(a1*(dt*dt)/2.0));
+        vec3 a2 = p->getForce()/m;
+        p->getVelocity().operator +=(dt/2.0*a2.operator +=(a1));
+        std::cout << "r" << p->getPosition() << "v" << p->getVelocity() << std::endl;
     }
 }
