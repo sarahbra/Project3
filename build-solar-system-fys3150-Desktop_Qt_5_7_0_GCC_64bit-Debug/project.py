0c534358fd44353cffd97b06c0c2c8a16cb8ac40
solveiaf <solveiaf@student.matnat.uio.no>
2016-10-24 17:38:22 +0200

    o

--- build-solar-system-fys3150-Desktop_Qt_5_7_0_GCC_64bit-Debug/project.py
+++ build-solar-system-fys3150-Desktop_Qt_5_7_0_GCC_64bit-Debug/project.py
@@ -7,18 +7,18 @@ filename = "positions.dat"
 arr=loadtxt('positions.dat',delimiter=',')
 print arr
 print arr[0][:-1]
-n = size(arr)/3
+n = size(arr)/4
 x = zeros(n)
 y = zeros(n)
 for i in range(n):
-	x[i] = arr[i][0]
-	y[i] = arr[i][1]
-	print x[i], y[i]
+	x[i] = arr[i][2]
+	y[i] = arr[i][3]
+	#print x[i], y[i]
 
 plot(x,y)
-legend('noe')
-title("Euler")
-xlabel("X")
-ylabel("Y")
-axis([-2,2,-2,2])
+#legend('noe')
+title("Euler Two Body Problem (dt=1e-05, n=100000)")
+xlabel("X [AU]")
+ylabel("Y [AU]")
+axis([-1.5,1.5,-1.5,1.5])
 show()
